5fb3345dd9d46133a3e63ad3f0247199398b8583
Luciano Almeida <passos.luciano@outlook.com>
2018-11-04 23:31:41 -0200

    Updating step

--- step.sh
+++ step.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-set -ex
+set -e
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+pushd $(mktemp -d)
+wget https://github.com/LucianoPAlmeida/variable-injector/releases/download/${variable_injector_version}/variable-injector-${variable_injector_version}-x86_64-apple-macosx10.10.zip
+unzip variable-injector-${variable_injector_version}-x86_64-apple-macosx10.10.zip
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+#Check verbose
+VERBOSE=""
+if [${verbose} = true ]; then
+    VERBOSE="--verbose"
+fi;
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+TEMP_DIR=$(pwd)
+popd
+
+${TEMP_DIR}/variable-injector-${variable_injector_version}-x86_64-apple-macosx10.10/variable-injector --file ${files_paths} --ignore ${vars_to_ignore} $VERBOSE
