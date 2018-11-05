39d5d0c9aefd863e549122dafedfa04cff89ecef
Luciano Almeida <passos.luciano@outlook.com>
2018-11-05 01:48:42 -0200

    Log verbose

--- step.sh
+++ step.sh
@@ -6,14 +6,16 @@ wget https://github.com/LucianoPAlmeida/variable-injector/releases/download/${va
 unzip x86_64-apple-macosx10.10.zip
 cp -f x86_64-apple-macosx10.10/release/variable-injector /usr/local/bin/variable-injector
 cp -f x86_64-apple-macosx10.10/release/libSwiftSyntax.dylib /usr/local/lib/libSwiftSyntax.dylib
+
 #Check verbose
 VERBOSE=""
 
 if [ ${verbose} = "true" ]; then
+
     VERBOSE="--verbose"
 fi;
 
 TEMP_DIR=$(pwd)
 popd
-
-variable-injector --file ${files_paths} --ignore ${vars_to_ignore} $VERBOSE
+echo "=> $VERBOSE"
+variable-injector --file ${files_paths} $VERBOSE --ignore ${vars_to_ignore} 
