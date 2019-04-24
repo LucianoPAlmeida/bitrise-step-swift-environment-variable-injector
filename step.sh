d4012de219c84fb565b2f47cedec74a9454e915b
Luciano Almeida <passos.luciano@outlook.com>
2019-04-23 21:48:52 -0300

    Updating script to build directly from git tag.

--- step.sh
+++ step.sh
@@ -2,13 +2,14 @@
 set -e
 
 pushd $(mktemp -d)
-wget https://github.com/LucianoPAlmeida/variable-injector/releases/download/${variable_injector_version}/x86_64-apple-macosx.zip
-unzip x86_64-apple-macosx.zip
-cp -f ./x86_64-apple-macosx/release/variable-injector /usr/local/bin/variable-injector
 
-rm ./x86_64-apple-macosx.zip
-rm -rf ./x86_64-apple-macosx
-rm -rf ./__MACOSX/
+git clone https://github.com/LucianoPAlmeida/variable-injector.git
+cd variable-injector
+git checkout ${variable_injector_version}
+git checkout -b ${variable_injector_version}-branch
+
+# Installing
+make install
 
 #Check verbose
 VERBOSE=""
